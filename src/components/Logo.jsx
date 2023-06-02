099fdc704f4d91bbcc8d1e208589dbe3f2c64207
Jhon Sisneros <jhonsisneros99@gmail.com>
2023-06-02 12:02:59 -0400

    initial commit; designed landing page

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+const Logo = () => {
+  return (
+    <div className="flex items-center text-6xl font-semibold text-sky-500 gap-x-2">
+      <div className="bg-sky-500 text-white flex justify-center p-2 rounded-md">
+        <h1 className="">W</h1>
+      </div>
+      <p>orkaholic</p>
+    </div>
+  );
+};
+export default Logo;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,4 @@
+const Navbar = () => {
+  return <div className="flex fixed">Navbar</div>;
+};
+export default Navbar;
